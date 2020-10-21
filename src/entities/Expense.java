2cebcc2625ff6e401554666b4691afc3028eb736
DaviMatheus <54643148+DaviMatheus@users.noreply.github.com>
2020-10-21 11:04:05 -0300

    Update Expense.java

--- src/entities/Expense.java
+++ src/entities/Expense.java
@@ -1,5 +1,39 @@
 package entities;
 
-public class Expense {
+public class Expense { private double expense;
+    private int month;
+    private int year;
+    
+    public Expense(Double expense, int month, int year) {
+		this.expense = expense;
+		this.month = month;
+		this.year = year;
+	}
+    
+    public double getexpense() {
+		return expense;
+	}
+
+	public void setexpense(double expense) {
+		this.expense = expense;
+	}
+
+    public int getmonth() {
+		return month;
+	}
+
+	public void setmonth(int month) {
+		this.month = month;
+	}
+
+    public int getyear() {
+		return year;
+	}
+
+	public void setyear(int year) {
+		this.year = year;
+	}
+
+}
 
 }
