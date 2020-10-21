d669c18be4fd235ec778eb27fab57efeb05ef635
Lais Portela <lais3722@gmail.com>
2020-10-21 15:07:18 -0300

    Criação da classe SubCategory  Mudanças a serem submetidas: 	new file:   src/entities/SubCategory.java

---
+++ src/entities/SubCategory.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class SubCategory {
+
+	private String subCategory;
+	
+	public SubCategory(String subCategory) {
+		this.subCategory= subCategory;
+	}
+
+	public String getSubCategory() {
+		return subCategory;
+	}
+
+	public void setSubCategory(String subCategory) {
+		this.subCategory = subCategory;
+	}
+	
+}
