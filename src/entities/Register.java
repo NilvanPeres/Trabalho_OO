4f40436b5392ca126b04a37f527fe470d7a4f40d
Nilvan Peres <nilvanperes@gmail.com>
2020-10-21 10:46:00 -0300

    Register Class atributes and methods

--- src/entities/Register.java
+++ src/entities/Register.java
@@ -2,5 +2,20 @@ package entities;
 
 public class Register {
 
-		
+	private Category category;
+
+	public Register(Category category) {
+		super();
+		this.category = category;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
 }
